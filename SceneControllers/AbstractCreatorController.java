7676f93f162c21ca12e443cc831f89d66ac1d7c9
Alexander Scheibe <ascheibe@Alexanders-MacBook-Pro.local>
2020-06-28 01:29:35 -0500

    search bar works

--- SceneControllers/AbstractCreatorController.java
+++ SceneControllers/AbstractCreatorController.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 import javafx.application.ConditionalFeature;
@@ -23,6 +26,7 @@ import javafx.scene.control.TitledPane;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import Code.*;
 
@@ -42,10 +46,23 @@ public abstract class AbstractCreatorController extends AbstractController {
     @FXML
     protected TitledPane resultsPane;
 
+    @FXML
+    protected Button one;
+
+    @FXML
+    protected Button two;
+
+    @FXML
+    protected Button three;
+
+    @FXML
+    protected Button four;
+
     private File textFile;
 
     private PrintWriter printWriter;
 
+    private List<Button> buttonList;
 
     /**
      * TODO: Sets up the ability for the search bar to detect input and 
@@ -59,19 +76,36 @@ public abstract class AbstractCreatorController extends AbstractController {
             @Override 
             public void changed(ObservableValue ov, String t, String t1) {                
                 System.out.println("Searching...");
-                
-                
 
+                buttonsPane.setVisible(false);
+                resultsPane.setVisible(true);
 
+                GridPane grid = new GridPane();
 
+                for(int i = 0; i < buttonList.size(); i++){
+                    if(buttonList.get(i).getText().contains(searchBar.getText())){
+                        grid.add(buttonList.get(i), 0, i);
+                    }
+                }
 
-
-
-
-
+                resultsPane.setContent(grid);
             }    
         });
     }
+
+    protected final void setButtonList(){
+
+        buttonList = new ArrayList<Button>();
+
+        System.out.println(textArea.getText());
+        System.out.println(one.getText());
+
+        buttonList.add(one);
+        buttonList.add(two);
+        buttonList.add(three);
+        buttonList.add(four);
+    }
+    
 
     /**
      * Sets the text file that is to be edited.
